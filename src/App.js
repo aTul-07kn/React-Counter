f5245195483f50407a7afd46c492a5338f26b69f
Atul Kumar Nayak <atulnayak7869@gmail.com>
2024-09-17 16:14:22 +0530

    counter app commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount]=useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='header'>
+        <h1>Local Counter App</h1>
       </header>
+
+      <h2>count with aTul: {count}</h2>
+      <button onClick={()=> setCount(0)}>RESET</button>
+      <button onClick={()=> count>=15? setCount(0): setCount(count+1)}>INCREASE</button>
+      <button onClick={()=> count<=0? setCount(0): setCount(count-1)}>DECREASE</button>
     </div>
-  );
+  )
 }
 
 export default App;
